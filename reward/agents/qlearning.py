e293c2553be7ac7ac7791dadb876ba56f35709b0
Paul Zhou <paul_zhou@brown.edu>
2022-08-09 18:07:47 -0400

    [BUGFIX] bug in msdm that does not initialize q values accordingly

--- reward/agents/qlearning.py
+++ reward/agents/qlearning.py
@@ -109,7 +109,7 @@ class QLearning(MSDMQLearning):
             ns = mdp.next_state_dist(s, a).sample(rng=rng)
             r = mdp.reward(s, a, ns)
             # update
-            q[s][a] += self.step_size*(r + mdp.discount_rate*max(q.get(ns, {0: 0}).values()) - q[s][a])
+            q[s][a] += self.step_size*(r + mdp.discount_rate*max(q[ns].values()) - q[s][a])
             # end of timestep
             for listener in event_listeners:
                 listener.end_of_timestep(locals())
