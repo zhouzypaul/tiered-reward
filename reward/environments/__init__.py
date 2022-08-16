780d20096ceef132654f135d5b36f2818eab65dc
Paul Zhou <paul_zhou@brown.edu>
2022-08-16 11:49:29 -0400

    remove simple grid from imports

--- reward/environments/__init__.py
+++ reward/environments/__init__.py
@@ -1,5 +1,4 @@
 from .chain import make_one_dim_chain
-from .simple_grid import make_simple_grid
 from .russell_norvig import make_russell_norvig_grid
 from .puddle_world import make_puddle_world
 from .frozen_lake import make_frozen_lake
